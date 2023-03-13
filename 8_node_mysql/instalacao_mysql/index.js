08bcd387c9b7132353b5f3983bd48578a76db268
luizanurnberg <nurnbergluiza@gmail.com>
2023-03-13 17:01:05 -0300

    oitava sessao

---
+++ 8_node_mysql/instalacao_mysql/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mysql = require('mysql')
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456789',
+    database: 'nodemysql2',
+})
+
+conn.connect(function (err) {
+    if(err){
+        console.log(err)
+    }
+    console.log('Conectou ao MySQL!')
+    app.listen(3000)
+})
