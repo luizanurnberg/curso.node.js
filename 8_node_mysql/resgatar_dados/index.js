59f48d9d80870d996b2f722865dbdc6c74559315
luizanurnberg <nurnbergluiza@gmail.com>
2023-03-14 08:50:46 -0300

    setima sessao

--- 8_node_mysql/resgatar_dados/index.js
+++ 8_node_mysql/resgatar_dados/index.js
@@ -20,11 +20,25 @@ app.post('/books/insertbook', (req, res) => {
     const title = req.body.title
     const pageqty = req.body.pageqty
     const sql = `INSERT INTO books (title, pageqty) VALUES ('${title}', '${pageqty}')`
-    conn.query(sql, function(err){
-        if(err){
+    conn.query(sql, function (err) {
+        if (err) {
             console.log(err)
         }
         res.redirect('/')
+    })
+})
+
+app.get('/books', (req, res) => {
+    const sql = 'SELECT * FROM books'
+
+    conn.query(sql, function (err, data) {
+        if(err){
+            console.log(err)
+            return 
+        }
+        const books = data 
+        console.log(books)
+        res.render('books', {books})
     })
 })
 
